a13565cafd91b3c1f6db5cc7c6fb0ac34eac41e0
unknown <ivanyuk.cergey@mail.ru>
2023-02-21 20:52:30 +0300

    feat: add function toReadable

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function toReadable (number) {
-  
+  let strNum = number.toString();
+  let units = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight',  'nine',  'ten',  'eleven',  'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen',  'eighteen', 'nineteen'];
+  let tens = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+  if (number === 0) return 'zero';
+  else if (number < 20) return units[number]; 
+  else if (number < 100) return (tens[strNum[0]] + ' ' + units[strNum[1]]).trim();
+  else if (number < 1000) {
+    if (strNum[1] === '0' && strNum[2] === '0') return units[strNum[0]] + ' hundred';
+    else return (units[strNum[0]] + ' hundred ' + toReadable(Number(strNum[1]) * 10 + Number(strNum[2]))).trim();
+  }
 }
